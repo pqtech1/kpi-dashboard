4a0bcb82fb92d1837298cdd67620d617a08aff31
Naimish Dwivedi <naimishdwivedi1307@gmail.com>
2025-12-20 15:33:36 +0530

    vite host add

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: "/kpi/",
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  server: {
+    host: true,
+    port: 8080,
+    allowedHosts: ["techupgrad.in", "www.techupgrad.in"],
   },
-}));
+});
